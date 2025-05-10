06e9b8f653220a0b7a866c9f284e7ffab674a83f
meusaas2025 <carlosborges325@gmail.com>
2025-05-10 03:31:19 -0300

    Clean up PDF export components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import GraficoComparativo from "./components/GraficoComparativo"
 import GraficoPizza from "./components/GraficoPizza"
-import ExportarPDF from "./components/ExportarPDF"
 import { Toaster } from "./components/Toaster"
 import BotaoDarkMode from "./components/BotaoDarkMode"
 import BotaoExportarPDF from "./components/BotaoExportarPDF"
@@ -9,8 +8,10 @@ export default function App() {
   return (
     <>
       <div id="painel-dashboard" className="p-4 bg-gray-100 dark:bg-zinc-800 min-h-screen">
-        <GraficoComparativo />
-        <GraficoPizza />
+        <div className="space-y-6">
+          <GraficoComparativo />
+          <GraficoPizza />
+        </div>
       </div>
       <BotaoExportarPDF />
       <BotaoDarkMode />
